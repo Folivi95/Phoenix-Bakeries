6837707569ee130f1273330b793528b8e1a5f805
Joshua Folivi <joshua.folivi@gmail.com>
2019-09-16 00:29:49 +0100

    Deployed to docker

--- Phoenix Bakeries/Controllers/BalanceController.cs
+++ Phoenix Bakeries/Controllers/BalanceController.cs
@@ -16,33 +16,30 @@ namespace Phoenix_Bakeries.Controllers
 
         public async Task<IActionResult> Index()
         {
-            //get account balance and display it
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {Environment.GetEnvironmentVariable("secretKey")}");
-
             try
             {
-
+                //get account balance and display it
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {Environment.GetEnvironmentVariable("publicKey")}");
+                string response = await client.GetStringAsync("https://api.paystack.co/balance");
+                BalanceAndTopUpViewModel row = JsonConvert.DeserializeObject<BalanceAndTopUpViewModel>(response);
+                //check if account balance returned a value
+                if (row.status)
+                {
+                    foreach (Models.Balance item in row.data)
+                    {
+                        ViewData["AccountBalance"] = string.Format("{0:n}", Convert.ToDecimal(item.balance) / 100);
+                    }
+                }
+                else
+                {
+                    ViewData["AccountBalance"] = "Not Available";
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error Occurred: {ex.Message}");
-            }
-            string response = await client.GetStringAsync("https://api.paystack.co/balance");
-            BalanceAndTopUpViewModel row = JsonConvert.DeserializeObject<BalanceAndTopUpViewModel>(response);
-
-            //check if account balance returned a value
-            if (row.status)
-            {
-                foreach (Models.Balance item in row.data)
-                {
-                    ViewData["AccountBalance"] = string.Format("{0:n}", Convert.ToDecimal(item.balance) / 100);
-                }
-            }
-            else
-            {
-                ViewData["AccountBalance"] = "Not Available";
             }
 
             client.Dispose();
